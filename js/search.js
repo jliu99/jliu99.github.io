b0bd3367e83658c2ab1c2dba7dbb8403cf18e707
jliu99 <jliu@ucls.uchicago.edu>
2016-08-08 11:26:06 -0500

    Implement search on other pages

--- js/search.js
+++ js/search.js
@@ -1,5 +1,6 @@
 function search() {
-	var term = document.getElementById(“query”).value;
+	var term = document.getElementById("query").value;
 	term = term.toLowerCase();
+    term = term.replace(" ", "+");
 	window.location.href = "http://jliu99.github.io/search.html?query=" + term;
 }
