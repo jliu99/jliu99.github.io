fc81cb25df9846a4cc60c13860d4e2a675a178c5
jliu99 <jliu@ucls.uchicago.edu>
2016-08-09 10:37:23 -0500

    Try to fix bar resize

--- js/signature-module.js
+++ js/signature-module.js
@@ -266,11 +266,16 @@ function resetBars() {
     var padding = n / 16.5;
     var maxRadius = 3 * n / 8;
     
-    scale.range([minRadius, maxRadius]);
+    scale.domain([0, maxScore])
+        .range([minRadius, maxRadius]);
+        
+    var rValues = [];
     
     for(i = 0; i < radiiValues.length; i++){
-        radiiValues[i] = scale(arraydata[i]);
+        rValues.push(scale(arraydata[i]));
     }
+    
+    radiiValues = rValues;
 
     for (i = 0; i < barN; i++) {
         var isTopResult = false;
