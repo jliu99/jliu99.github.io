5cbc909e54490823af1ee7ced0be2ec21752f257
jliu99 <jliu@ucls.uchicago.edu>
2016-08-10 14:14:58 -0500

    Add key listener

--- js/search.js
+++ js/search.js
@@ -1,6 +1,17 @@
+document.getElementById('query').onkeypress = function (e) {
+    if (!e) e = window.event;
+    var keyCode = e.keyCode || e.which;
+    if (keyCode == '13') {
+        // Enter pressed
+        search();
+    }
+}
+
 function search() {
     var term = document.getElementById("query").value;
-    term = term.toLowerCase();
-    term = term.replace(" ", "+");
-    window.location.href = "http://jliu99.github.io/search.html?query=" + term;
+    if (term != null || term.replace(" ", "") != "") {
+        term = term.toLowerCase();
+        term = term.replace(" ", "+");
+        window.location.href = "http://jliu99.github.io/search.html?query=" + term;
+    }
 }
